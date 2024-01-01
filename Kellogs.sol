77123ae5504cb4ba511292e854a16a60667fa8b3
praneelpp <142261163+praneelpp@users.noreply.github.com>
2024-01-01 14:06:34 +0530

    Update Kellogs.sol

--- Kellogs.sol
+++ Kellogs.sol
@@ -2,10 +2,9 @@
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Kellogs is ERC20, ERC20Burnable, Ownable {
+contract Kellogs is ERC20, Ownable {
     constructor(address initialOwner)
         ERC20("kellogs", "KLG")
         Ownable(initialOwner)
@@ -13,5 +12,12 @@ contract Kellogs is ERC20, ERC20Burnable, Ownable {
 
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
+    }
+    function burn(uint256 value) public{
+        _burn(msg.sender, value);
+    }
+    function transfer(address to, uint256 value) public virtual override returns(string memory){
+        _transfer(to, value);
+        return true;
     }
 }
